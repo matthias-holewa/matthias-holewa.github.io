71a56b1e005e42aa7fbaef3b225ab00f3bebb06b
Matti <77966696+matthias-holewa@users.noreply.github.com>
2022-02-27 19:01:27 +0100

    img upload & reorg

--- assets/js/main.js
+++ assets/js/main.js
@@ -127,7 +127,7 @@
 
 		// Toggle.
 			$navPanelToggle = $(
-				'<a href="#navPanel" id="navPanelToggle">Menu</a>'
+				'<a href="#navPanel" id="navPanelToggle">Navigation</a>'
 			)
 				.appendTo($wrapper);
 
